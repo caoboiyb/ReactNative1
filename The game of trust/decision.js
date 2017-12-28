e225ed0aab754c4f2fd888309190d06c91e61f77
trungducng <caoboiyb1997@gmail.com>
2017-12-29 05:51:48 +0700

    Try Simpleton

--- The game of trust/decision.js
+++ The game of trust/decision.js
@@ -4,11 +4,24 @@
 //   { yours: true, theirs: false },
 // ]
 const decide = (history = []) => {
-  return history.length == 0 ? true : (() => {
+  return history.length == 0 ? true : () => {
     const lastChild = history.length - 1;
-    return history[lastChild].theirs;
-  })()
+    history[lastChild].theirs == true ? history[lastChild].yours : !history[lastChild].yours
+  }
 }
 
+// const copyCat = (history = []) => {
+//   return history.length == 0 ? true : (() => {
+//     const lastChild = history.length - 1;
+//     return history[lastChild].theirs;
+//   })()
+// }
+
+// const simpleTon = (history = []) => {
+//   return history.length == 0 ? true : () => {
+//     const lastChild = history.length - 1;
+//     history[lastChild].theirs == true ? history[lastChild].yours : !history[lastChild].yours
+//   }
+// }
 
 module.exports = decide;
