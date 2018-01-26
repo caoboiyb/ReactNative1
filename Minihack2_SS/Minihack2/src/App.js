2ad29559fb7d24533c0dc9a9c1b2c2599cd02c52
trungducng <caoboiyb1997@gmail.com>
2018-01-26 10:46:53 +0700

    Hw done

--- Minihack2_SS/Minihack2/src/App.js
+++ Minihack2_SS/Minihack2/src/App.js
@@ -13,26 +13,62 @@ const unit = ["Metre", "Decimetre", "Centimetre", "Milimetre"]
 
 class App extends PureComponent {
 
-  state = { 
-    left: {
-      currentUnit: "Metre",
-      value: "0"
-    },
-    right: {
-      currentUnit: "Metre",
-      value: "0"
+  state = {
+    leftIndex: "0",
+    leftValue: "0",
+    rightIndex: "0",
+    rightValue: "0"
+  }
+
+
+
+  _onChangeInput = (value, pos) => {
+    if (pos == "0"){
+      const pow = this.state.rightIndex - this.state.leftIndex;
+      this.setState({
+        leftValue: value,
+        rightValue: String(Number(value) *(10**pow))
+      })
+    } else {
+      const pow = this.state.leftIndex - this.state.rightIndex;
+      this.setState({
+        rightValue: value,
+        leftValue: String(Number(value) *(10**pow))
+      })
     }
-   }
+  }
 
-   _onChangeInput = (unit, value) => {
-
-   }
+  _onChangeUnit = (index, pos) => {
+    if (pos == "0"){
+      const pow = this.state.rightIndex - index;
+      this.setState({
+        leftIndex: index,
+        rightValue: String(Number(this.state.leftValue) *(10**pow))
+      })
+    } else {
+      const pow = this.state.leftIndex - index;
+      this.setState({
+        rightIndex: index,
+        leftValue: String(Number(this.state.rightValue) *(10**pow))
+      })
+    }
+  }
 
   render() {
     return (
-      <View style={{flex: 1, flexDirection: "row", backgroundColor: "#71589F", marginTop: 20}}>
-        <ColumnView onChangeInput={this._onChangeInput}/>
-        <ColumnView onChangeInput={this._onChangeInput}/>
+      <View style={{ flex: 1, flexDirection: "row", backgroundColor: "#71589F", marginTop: 20 }}>
+        <ColumnView 
+          onChangeInput={this._onChangeInput} 
+          onChangeUnit={this._onChangeUnit} 
+          position="0" 
+          unit={unit[this.state.leftIndex]} 
+          value={this.state.leftValue} />
+        <ColumnView 
+          onChangeInput={this._onChangeInput} 
+          onChangeUnit={this._onChangeUnit} 
+          position="1" 
+          unit={unit[this.state.rightIndex]} 
+          value={this.state.rightValue} />
       </View>
     );
   }
--- Minihack2_SS/Minihack2/src/containers/ColumnView.js
+++ Minihack2_SS/Minihack2/src/containers/ColumnView.js
@@ -9,37 +9,42 @@ import {
 
 import Selector from "../components/Selector"
 
+const unit = ["Metre", "Decimetre", "Centimetre", "Milimetre"]
+
 class ColumnView extends PureComponent {
     state = {
-        currentUnit: "Metre",
-        unit: ["Metre", "Decimetre", "Centimetre", "Milimetre"],
-        value: "125"
+        value: "0"
     }
 
     _onPress = (index) => {
-        this.setState({
-            currentUnit: this.state.unit[index]
-        })
+        this.props.onChangeUnit(index, this.props.position)
     }
 
     _onChangeText = (text) => {
         this.setState({
             value: text
+        }, this.props.onChangeInput(text, this.props.position))
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            value: nextProps.value
         })
     }
+
 
     render() {
         return (
             <View style={styles.wrapper}>
                 <View style={{ backgroundColor: "#4E3D6E" }}>
-                    <Text style={styles.text}>{this.state.currentUnit}</Text>
-                    <TextInput onChangeText={this._onChangeText} style={[styles.text, {paddingBottom: 20}]} value={this.state.value} />
+                    <Text style={styles.text}>{this.props.unit}</Text>
+                    <TextInput onChangeText={this._onChangeText} style={[styles.text, { paddingBottom: 20 }]} value={this.state.value} />
                 </View>
                 <View style={{ flex: 1, flexDirection: "column" }}>
-                    <Selector onPress={() => this._onPress(0)} backgroundColor="#5D4883" name={this.state.unit[0]} />
-                    <Selector onPress={() => this._onPress(1)} backgroundColor="#6A5295" name={this.state.unit[1]} />
-                    <Selector onPress={() => this._onPress(2)} backgroundColor="#5D4883" name={this.state.unit[2]} />
-                    <Selector onPress={() => this._onPress(3)} backgroundColor="#6A5295" name={this.state.unit[3]} />
+                    <Selector onPress={() => this._onPress(0)} backgroundColor="#5D4883" name={unit[0]} />
+                    <Selector onPress={() => this._onPress(1)} backgroundColor="#6A5295" name={unit[1]} />
+                    <Selector onPress={() => this._onPress(2)} backgroundColor="#5D4883" name={unit[2]} />
+                    <Selector onPress={() => this._onPress(3)} backgroundColor="#6A5295" name={unit[3]} />
                 </View>
             </View>
         );
@@ -51,9 +56,9 @@ const styles = StyleSheet.create({
         flex: 1
     },
     text: {
-        color: "#FFFFFF", 
-        fontSize: 24, 
-        paddingTop: 20, 
+        color: "#FFFFFF",
+        fontSize: 24,
+        paddingTop: 20,
         marginLeft: 10
     }
 })
